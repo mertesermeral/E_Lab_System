73648f6d301c1fec4593fbbed5758ff4e18c5554
Mert Eser Meral <eser.meral@ogr.sakarya.edu.tr>
2025-01-05 17:22:58 +0300

    admin dashboard hızlı değerlendirme eklendi

--- Admin/AdminDashboard.js
+++ Admin/AdminDashboard.js
@@ -1,6 +1,8 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { createDrawerNavigator } from "@react-navigation/drawer";
-import { View, Text, StyleSheet, TouchableOpacity, Image, Alert, BackHandler } from "react-native";
+import { View, Text, TextInput, ScrollView, StyleSheet, TouchableOpacity, Alert, BackHandler } from "react-native";
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "../firebase";
 import Kilavuz from "./Kilavuz";
 import KilavuzList from "./KilavuzList";
 import TahlilEkle from "./TahlilEkle";
@@ -8,64 +10,253 @@ import TahlilList from "./TahlilList";
 
 const Drawer = createDrawerNavigator();
 
-const DashboardHome = ({ navigation }) => {
-  // Geri tuşu kontrolü
+// Serum değerini değerlendirme fonksiyonu
+const evaluateSerumValue = (serumValue, min, max) => {
+  if (serumValue < min) return "↓"; // düşük
+  if (serumValue > max) return "↑"; // yüksek
+  return "↔"; // normal
+};
+
+// Okların hizalanması
+const getArrowWithAlignment = (rangeText, arrow) => {
+    const [min, max] = rangeText.split(/(?<!^)-/);
+
+  switch (arrow) {
+    case "↓":
+      return (
+        <Text style={{ color: "green", fontWeight: "bold" }}>
+          {arrow} {rangeText}
+        </Text>
+      );
+    case "↔":
+      return (
+        <Text style={{  color: "blue", fontWeight: "bold" }}>
+          {min.trim()} {arrow} {max.trim()}
+        </Text>
+      );
+    case "↑":
+      return (
+        <Text style={{  color: "red", fontWeight: "bold" }}>
+          {rangeText} {arrow}
+        </Text>
+      );
+    default:
+      return <Text>{rangeText}</Text>;
+  }
+};
+
+const DashboardHome = ({navigation}) => {
   useEffect(() => {
     const backAction = () => {
-      if (navigation.isFocused()) { // Yalnızca bu sayfa odaklıysa
+      if (navigation.isFocused()) {
         Alert.alert("Uyarı", "Oturumdan çıkmak istiyor musunuz?", [
-          {
-            text: "Hayır",
-            onPress: () => null,
-            style: "cancel",
-          },
-          {
-            text: "Evet",
-            onPress: () => navigation.replace("Home"),
-          },
+          { text: "Hayır", style: "cancel" },
+          { text: "Evet", onPress: () => navigation.replace("Home") },
         ]);
-        return true; // İşlemi engelle
+        return true;
       }
-      return false; // Diğer ekranlarda varsayılan davranış
+      return false;
     };
 
     const backHandler = BackHandler.addEventListener("hardwareBackPress", backAction);
-
-    return () => backHandler.remove(); // Bileşen unmount olunca listener'ı kaldır
+    return () => backHandler.remove();
   }, [navigation]);
 
+  const [birthDate, setBirthDate] = useState("");
+  const [age, setAge] = useState(0);
+  const [serumValues, setSerumValues] = useState({
+    IgG: "",
+    IgA: "",
+    IgM: "",
+    IgG1: "",
+    IgG2: "",
+    IgG3: "",
+    IgG4: "",
+    IgA1: "",
+    IgA2: "",
+  });
+  const [guides, setGuides] = useState([]);
+  const [evaluationResults, setEvaluationResults] = useState([]);
+
+  useEffect(() => {
+    const fetchGuides = async () => {
+      try {
+        const querySnapshot = await getDocs(collection(db, "guides"));
+        const guidesData = [];
+        querySnapshot.forEach((doc) => guidesData.push({ guideName: doc.id, data: doc.data().data }));
+        setGuides(guidesData);
+      } catch (error) {
+        Alert.alert("Hata", "Kılavuzlar alınırken bir hata oluştu.");
+      }
+    };
+
+    fetchGuides();
+  }, []);
+
+  const calculateAgeInMonths = (birthDateStr) => {
+    const parts = birthDateStr.split("/");
+    if (parts.length !== 3) return 0;
+    const birthDate = new Date(`${parts[2]}-${parts[1]}-${parts[0]}`);
+    const currentDate = new Date();
+    return (currentDate.getFullYear() - birthDate.getFullYear()) * 12 + (currentDate.getMonth() - birthDate.getMonth());
+  };
+
+  const handleBirthDateChange = (value) => {
+    const formattedValue = value.replace(/[^0-9]/g, "").replace(/(\d{2})(\d{2})(\d{4})/, "$1/$2/$3");
+    setBirthDate(formattedValue);
+    if (formattedValue.length === 10) {
+      const calculatedAge = calculateAgeInMonths(formattedValue);
+      setAge(calculatedAge);
+    }
+  };
+
+  const handleEvaluate = () => {
+    if (!birthDate) {
+      Alert.alert("Hata", "Lütfen doğum tarihini girin!");
+      return;
+    }
+
+    const serumValuesArray = Object.entries(serumValues)
+      .filter(([_, value]) => value.trim() !== "") // Boş serum değerlerini filtrele
+      .map(([type, value]) => ({
+        serumType: type,
+        value: parseFloat(value),
+      }));
+
+    const results = guides.map((guide) => {
+      const filteredRows = guide.data.filter((row) => {
+        const [minAge, maxAge] = row.ageRange.split("-").map(Number);
+        return (!minAge || age >= minAge) && (!maxAge || age <= maxAge);
+      });
+
+      const evaluations = filteredRows.flatMap((row) =>
+        serumValuesArray
+          .map((serum) => {
+            if (row.serumType === serum.serumType) {
+              const geoMeanEvaluation = isNaN(row.geoMeanMin) || isNaN(row.geoMeanMax)
+                ? null
+                : evaluateSerumValue(serum.value, row.geoMeanMin, row.geoMeanMax);
+
+              const arithMeanEvaluation = isNaN(row.arithMeanMin) || isNaN(row.arithMeanMax)
+                ? null
+                : evaluateSerumValue(serum.value, row.arithMeanMin, row.arithMeanMax);
+
+              const meanEvaluation = isNaN(row.meanMin) || isNaN(row.meanMax)
+                ? null
+                : evaluateSerumValue(serum.value, row.meanMin, row.meanMax);
+
+              const intervalEvaluation = isNaN(row.intervalMin) || isNaN(row.intervalMax)
+                ? null
+                : evaluateSerumValue(serum.value, row.intervalMin, row.intervalMax);
+
+              const minmaxEvaluation = isNaN(row.min) || isNaN(row.max)
+                ? null
+                : evaluateSerumValue(serum.value, row.min, row.max);
+
+              return {
+                serumType: serum.serumType,
+                geoMean: geoMeanEvaluation ? `${row.geoMeanMin}-${row.geoMeanMax}` : null,
+                geoMeanArrow: geoMeanEvaluation,
+                arithMean: arithMeanEvaluation ? `${row.arithMeanMin}-${row.arithMeanMax}` : null,
+                arithMeanArrow: arithMeanEvaluation,
+                mean: meanEvaluation ? `${row.meanMin}-${row.meanMax}` : null,
+                meanArrow: meanEvaluation,
+                interval: intervalEvaluation ? `${row.intervalMin}-${row.intervalMax}` : null,
+                intervalArrow: intervalEvaluation,
+                minmax: minmaxEvaluation ? `${row.min}-${row.max}` : null,
+                minmaxArrow: minmaxEvaluation,
+              };
+            }
+            return null;
+          })
+          .filter(Boolean)
+      );
+
+      return evaluations.length ? { guideName: guide.guideName, ageRange: filteredRows[0]?.ageRange || "-", evaluations } : null;
+    }).filter(Boolean);
+
+    setEvaluationResults(results);
+  };
+
   return (
-    <View style={styles.container}>
-      <Image source={require("../assets/doctor.png")} style={styles.icon} />
-      <Text style={styles.title}>Doktor Sayfası</Text>
-      <TouchableOpacity
-        style={styles.button}
-        onPress={() => navigation.navigate("Kilavuz")}
-      >
-        <Text style={styles.buttonText}>Kılavuz Oluşturma</Text>
+    <ScrollView contentContainerStyle={styles.scrollContainer}>
+      <Text style={styles.title}>Hızlı Değerlendirme</Text>
+
+      <TextInput
+        style={styles.input}
+        placeholder="Doğum Tarihi (GG/AA/YYYY)"
+        value={birthDate}
+        onChangeText={handleBirthDateChange}
+      />
+      <Text style={styles.ageDisplay}>Yaş (Ay): {age}</Text>
+
+      <Text style={styles.subTitle}>Serum Değerleri:</Text>
+      {Object.keys(serumValues).map((serumType) => (
+        <TextInput
+          key={serumType}
+          style={styles.input}
+          placeholder={`${serumType} Değeri`}
+          keyboardType="numeric"
+          value={serumValues[serumType]}
+          onChangeText={(value) =>
+            setSerumValues((prev) => ({ ...prev, [serumType]: value }))
+          }
+        />
+      ))}
+
+      <TouchableOpacity style={styles.evaluateButton} onPress={handleEvaluate}>
+        <Text style={styles.ButtonText}>Değerlendir</Text>
       </TouchableOpacity>
-      <TouchableOpacity
-        style={styles.button}
-        onPress={() => navigation.navigate("KilavuzList")}
-      >
-        <Text style={styles.buttonText}>Kılavuz Listeleme</Text>
-      </TouchableOpacity>
-      <TouchableOpacity
-        style={styles.button}
-        onPress={() => navigation.navigate("TahlilEkle")}
-      >
-        <Text style={styles.buttonText}>Tahlil Ekle</Text>
-      </TouchableOpacity>
-      <TouchableOpacity
-        style={styles.button}
-        onPress={() => navigation.navigate("TahlilList")}
-      >
-        <Text style={styles.buttonText}>Tahlil Listeleme</Text>
-      </TouchableOpacity>
-    </View>
+
+      <Text style={styles.subTitle}>Kılavuzlara Göre Değerlendirmeler</Text>
+      {evaluationResults.length === 0 && <Text style={styles.infoText}>Henüz değerlendirme yapılmadı.</Text>}
+      {evaluationResults.map((result, index) => (
+        <View key={index} style={styles.tableContainer}>
+          <Text style={styles.tableHeader}>{result.guideName}</Text>
+          <Text style={styles.tableSubHeader}>Yaş Aralığı: {result.ageRange}</Text>
+          <View style={styles.tableRowHeader}>
+            <Text style={styles.tableCellHeader}>Serum Tipi</Text>
+            {result.evaluations.some((e) => e.geoMean) && <Text style={styles.tableCellHeader}>GeoMean</Text>}
+            {result.evaluations.some((e) => e.arithMean) && <Text style={styles.tableCellHeader}>ArithMean</Text>}
+            {result.evaluations.some((e) => e.mean) && <Text style={styles.tableCellHeader}>Mean</Text>}
+            {result.evaluations.some((e) => e.minmax) && <Text style={styles.tableCellHeader}>Min-Max</Text>}
+            {result.evaluations.some((e) => e.interval) && <Text style={styles.tableCellHeader}>Interval</Text>}
+          </View>
+          {result.evaluations.map((evaluation, evalIndex) => (
+            <View key={evalIndex} style={styles.tableRow}>
+              <Text style={styles.tableCell}>{evaluation.serumType}</Text>
+              {evaluation.geoMean && (
+                <Text style={styles.tableCell}>
+                  {getArrowWithAlignment(evaluation.geoMean, evaluation.geoMeanArrow)}
+                </Text>
+              )}
+              {evaluation.arithMean && (
+                <Text style={styles.tableCell}>
+                  {getArrowWithAlignment(evaluation.arithMean, evaluation.arithMeanArrow)}
+                </Text>
+              )}
+              {evaluation.mean && (
+                <Text style={styles.tableCell}>
+                  {getArrowWithAlignment(evaluation.mean, evaluation.meanArrow)}
+                </Text>
+              )}
+              {evaluation.minmax && (
+                <Text style={styles.tableCell}>
+                  {getArrowWithAlignment(evaluation.minmax, evaluation.minmaxArrow)}
+                </Text>
+              )}
+              {evaluation.interval && (
+                <Text style={styles.tableCell}>
+                  {getArrowWithAlignment(evaluation.interval, evaluation.intervalArrow)}
+                </Text>
+              )}
+            </View>
+          ))}
+        </View>
+      ))}
+    </ScrollView>
   );
 };
-
 const AdminDashboard = () => {
   return (
     <Drawer.Navigator
@@ -102,39 +293,81 @@ const AdminDashboard = () => {
     </Drawer.Navigator>
   );
 };
-
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
+  input: { borderWidth: 1, borderColor: "#ccc", padding: 10, marginBottom: 10, borderRadius: 8 },
+  ageDisplay: { fontSize: 16, marginBottom: 10 },
+  evaluateButton: { backgroundColor: "green", padding: 15, borderRadius: 8 },
+  infoText: { textAlign: "center", marginVertical: 20, color: "#777" },
+  scrollContainer: {
     padding: 20,
-    backgroundColor: "#f7f7f7",
+    backgroundColor: "#fff",
   },
   title: {
     fontSize: 28,
     fontWeight: "bold",
-    color: "#0058a3",
+    color: "#0058a3", // Ana başlık rengi
     textAlign: "center",
-    marginBottom: 30,
+    marginBottom: 20,
   },
-  button: {
-    width: "100%",
-    backgroundColor: "#0058a3",
-    padding: 15,
-    borderRadius: 8,
-    alignItems: "center",
-    marginVertical: 10,
+  subTitle: {
+    fontSize: 23,
+      fontWeight: "bold",
+      color: "#0058a3", // Ana başlık rengi
+      textAlign: "center",
+      marginBottom: 10,
+      marginVertical: 10,
   },
-  buttonText: {
+  ButtonText: {
     color: "#fff",
-    fontSize: 16,
+    textAlign: "center",
     fontWeight: "bold",
   },
-  icon: {
-    resizeMode: "contain",
+  //yeni ekleme
+  tableContainer: {
     marginBottom: 20,
+    borderWidth: 1,
+    borderColor: "#ccc",
+    borderRadius: 8,
+    backgroundColor: "#f9f9f9",
     padding: 10,
+  },
+  tableHeader: {
+    fontSize: 20,
+    fontWeight: "bold",
+    textAlign: "center",
+    color: "#0058a3", // Başlık rengi
+    marginBottom: 10,
+  },
+  tableSubHeader: {
+    fontSize: 16,
+    fontWeight: "600",
+    marginBottom: 10,
+    color: "#0058a3", // Alt başlık rengi
+  },
+  tableRowHeader: {
+    flexDirection: "row",
+    backgroundColor: "#0058a3", // Tablo başlık arka plan rengi
+    paddingVertical: 8,
+    borderRadius: 5,
+  },
+  tableRow: {
+    flexDirection: "row",
+    borderBottomWidth: 1,
+    borderColor: "#ccc",
+    paddingVertical: 8,
+  },
+  tableCellHeader: {
+    flex: 1,
+    fontWeight: "bold",
+    textAlign: "center",
+    fontSize: 14,
+    color: "#fff", // Başlık metin rengi
+  },
+  tableCell: {
+    flex: 1,
+    textAlign: "center",
+    fontSize: 14,
+    color: "#333",
   },
 });
 
