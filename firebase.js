7c6e352c387425d68e80988fb28075f3e1f511ba
Mert Eser Meral <eser.meral@ogr.sakarya.edu.tr>
2025-01-05 11:55:31 +0300

    bug fixed

--- firebase.js
+++ firebase.js
@@ -1,10 +1,16 @@
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-import { API_KEY, AUTH_DOMAIN, PROJECT_ID, STORAGE_BUCKET, MESSAGING_SENDER_ID, APP_ID, MEASUREMENT_ID } from "@env";
+import {
+  API_KEY,
+  AUTH_DOMAIN,
+  PROJECT_ID,
+  STORAGE_BUCKET,
+  MESSAGING_SENDER_ID,
+  APP_ID,
+  MEASUREMENT_ID,
+} from "@env";
 
-// Firebase yapılandırması
 const firebaseConfig = {
   apiKey: API_KEY,
   authDomain: AUTH_DOMAIN,
@@ -17,8 +23,7 @@ const firebaseConfig = {
 
 // Firebase Bağlantısı
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
- const auth = getAuth(app);
+const auth = getAuth(app);
 const db = getFirestore(app);
 
-export {auth,db}
+export { auth, db };
