0ca12d51cdb34d17aa1996a597001ae1e990769a
Mert Eser Meral <eser.meral@ogr.sakarya.edu.tr>
2025-01-30 17:32:32 +0300

    build error

---
+++ config.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+    apiKey: "***REMOVED***",
+    authDomain: "***REMOVED***",
+    databaseURL: "https://***REMOVED***-default-rtdb.firebaseio.com",
+    projectId: "***REMOVED***",
+    storageBucket: "***REMOVED***.firebasestorage.app",
+    messagingSenderId: "***REMOVED***",
+    appId: "1:***REMOVED***:web:fb162d1d64641579fb3d4b",
+    measurementId: "***REMOVED***"
+  };
